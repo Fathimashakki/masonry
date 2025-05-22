c1e61560a64594c0b0fee5c25f6fd2186cff6ea6
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-22 16:26:46 +0530

    Initial Commit

---
+++ masonry/masonryApp.swift
@@ -0,0 +1,17 @@
+//
+//  masonryApp.swift
+//  masonry
+//
+//  Created by Shaan on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct masonryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
